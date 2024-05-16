e193bf6f2862f0959c93ad38eb6a1a4e39625862
Harish Babu <babu@siemens.com>
2024-05-16 20:34:25 +0530

    diff() added

--- MyCalc.java
+++ MyCalc.java
@@ -5,9 +5,15 @@ public class MyCalc
         return a+b;
     }
 
+    int diff(int a, int b)
+    {
+        return a-b;
+    }
+
     public static void main(String[] args)
     {
         MyCalc calc = new MyCalc();
         System.out.println("Sum is : " + calc.sum(20, 30));
+        System.out.println("Diff is : " + calc.diff(50, 30));
     }
 }
